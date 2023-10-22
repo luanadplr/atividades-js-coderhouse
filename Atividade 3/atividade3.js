8ee7f98c316a6880a0aff7bfbf469227fcb64113
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2023-10-22 17:34:59 -0300

    atividade-3-concluida

---
+++ Atividade 3/atividade3.js
@@ -0,0 +1,12 @@
+
+
+function calculoDolar(){
+    var cotacao = (a) => a*5.0528
+    var spread = (a) => a+(a*0.04)
+    var iof = (a) => a+(a*0.0638)
+    var preco = parseInt(prompt("Digite o valor da sua compra (USD):"))
+    preco = cotacao(preco)
+    preco = spread(preco)
+    preco = iof(preco)
+    alert(`Sua compra em reais será aproximadamente: R$${preco.toFixed(2)}\nOBS.: Este valor não conta taxas do cartão.`)
+}
