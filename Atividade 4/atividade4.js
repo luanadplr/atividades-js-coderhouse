eccfdea988e106ba9c54e1f238ca7772f9273479
Luana de Paula <51202496+luanadplr@users.noreply.github.com>
2023-10-25 19:39:08 -0300

    ajustes-ativ-4

--- Atividade 4/atividade4.js
+++ Atividade 4/atividade4.js
@@ -2,10 +2,10 @@ var resultado = document.querySelector("#resultado")
 function saoMultiplos(){
     var n1 = parseInt(document.querySelector("#n1").value)
     var n2 = parseInt(document.querySelector("#n2").value)
-    if(n1%n2==0){
+    if(n1%n2===0 || n2%n1===0){
         resultado.innerHTML = `Os números ${n1} e ${n2} são múltiplos.`
     } else{
-        resultado.append(`Os números ${n1} e ${n2} NÃO são múltiplos.`)
+        resultado.innerHTML(`Os números ${n1} e ${n2} NÃO são múltiplos.`)
     }
 }
 
