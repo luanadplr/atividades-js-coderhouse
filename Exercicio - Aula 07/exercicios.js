4432909a842de007e874c927cd930a3d8fb37d6c
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2023-10-31 17:48:00 -0300

    exercicios-aula07

---
+++ Exercicio - Aula 07/exercicios.js
@@ -0,0 +1,9 @@
+var div = document.querySelector("#ex1-res")
+function parImpar(){
+    var n1 = document.querySelector("#ex1-numero")
+    div.style.cssText = 
+        'display:block;'
+    if (n1%2==0){
+            
+    }
+}
